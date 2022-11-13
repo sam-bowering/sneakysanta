b6bd1cc7fe01c05925d2469d6a5880654d9d98b5
sam-bowering <samjbowering@gmail.com>
2022-11-13 19:58:53 +1300

    Adding names to list

--- client/components/Calculator.jsx
+++ client/components/Calculator.jsx
@@ -4,19 +4,27 @@ import { Form, Button } from 'semantic-ui-react'
 
 class Calculator extends React.Component {
   state = {
-    submittedName: '',
     names: []
   }
 
-  handleInputName = (e) => {
-    this.setState({ submittedName: e.target.value})
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value})
+  }
+
+  handleSubmitName = (state) => {
+    // store, set, apply
+    const { names, inputName } = state
+    const updatedNames = [...names]
+    updatedNames.push(inputName)
+    this.setState({ names: updatedNames})
   }
 
   render () {
     return (
       <div className='calculator'>
         <Form>
-          <Form.Input name='submitName' value={this.state.submittedname} onChange={this.handleInputName}></Form.Input>
+          <Form.Input name='inputName' value={this.state.submittedname} onChange={this.handleChange}></Form.Input>
+          <Button type='button' onClick={() => this.handleSubmitName(this.state)}>Add</Button>
         </Form>
       </div>
     )
