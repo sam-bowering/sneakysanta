180b8e2fe360ce122f91ecfe5df575d32187708a
sam-bowering <samjbowering@gmail.com>
2022-11-14 11:03:22 +1300

    Scrambling works as long as random num doesn't push scrambled number over 26

--- client/components/Name.jsx
+++ client/components/Name.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
 
 class Name extends React.Component {
+  componentDidMount() {
+    // input name -> split into letters
+    // -> assign name a number -> turn letters into respective number
+    // -> add number to each "letter" -> turn numbers back into letters
+    // -> return scrambled name
+
+    // ***
+
+    // assign number 
+    const scrambleNum = Math.floor(Math.random() * (10 - 1 + 1)) + 1
+    this.setState({ scrambleNum })
+
+    // letters to numbers
+    const lettersToNum = (letter) => {
+      return parseInt(letter, 36) - 9
+    }
+    
+    // testing splitting and converting to numbers in one
+    let scrambledName = this.props.name.split('').map(lettersToNum)
+
+    // add scramble number to number form of name
+    scrambledName = scrambledName.map(number => number += scrambleNum)
+
+    // convert back into letters
+    scrambledName = scrambledName.map(number => String.fromCharCode(number + 64))
+
+    this.setState({ scrambledName })
+    // ***
+
+    // scrambledName = scrambledName.map(letter =>
+    //   letter = 'x')
+    // this.setState({ scrambledName: scrambledName})
+  }
+
   state = {
-    isScrambledVisible: false
+    isScrambledVisible: false,
+    scrambledName: '',
+    scrambleNum: 0
   }
 
   handleClick = () => {
@@ -19,7 +55,7 @@ class Name extends React.Component {
         <div className='clean-name' onClick={this.handleClick}>{this.props.name}</div>
         {this.state.isScrambledVisible &&
           <div className='scrambled-name-container'>
-            <div className='scrambled-name'>{this.props.name}</div>
+            <div className='scrambled-name'>{this.state.scrambledName}</div>
             <p></p>
           </div>
         }
